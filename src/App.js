7fb5dc2f2fa0c59709a61a2c959d322fbdd34673
Sriram Mahalingam <smahali1@ford.com>
2020-10-19 07:17:53 -0400

    added right side table, data is flowing from live call

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import './App.css';
 import { FormControl, Select, MenuItem, Card, CardContent } from "@material-ui/core";
 import InfoBox from './InfoBox';
 import Map from './Map';
+import Table from './Table';
 function App() {
 
   const [countries, setCountries] = useState([]);
   const [country, setCountry] = useState('worldwide');
   const [countryInfo, setCountryInfo] = useState([]);
+  const [tableData, setTableData] = useState([]);
 
   useEffect(() => {
     fetch('https://disease.sh/v3/covid-19/all' )
@@ -37,6 +39,7 @@ function App() {
               value: country.countryInfo.iso2
             }
           ));
+          setTableData(data);
           setCountries(countries);
         });
     };
@@ -102,6 +105,7 @@ function App() {
       <Card className="app__right">
         <CardContent>
           <h3> Live Cases by Country</h3>
+          <Table countries={tableData}/>
           <h3> Worldwide new cases</h3>
           {/* Table */}
           {/* Graph */}
