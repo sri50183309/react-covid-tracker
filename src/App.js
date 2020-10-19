62bc6fb3fe127cb03979de0de88f2773825798b6
Sriram Mahalingam <smahali1@ford.com>
2020-10-19 10:37:50 -0400

    added linegraph in the right side

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import { FormControl, Select, MenuItem, Card, CardContent } from "@material-ui/c
 import InfoBox from './InfoBox';
 import Map from './Map';
 import Table from './Table';
+import { sortData } from './util';
+import LineGraph from './LineGraph';
 function App() {
 
   const [countries, setCountries] = useState([]);
@@ -39,7 +41,9 @@ function App() {
               value: country.countryInfo.iso2
             }
           ));
-          setTableData(data);
+
+          const sortedData = sortData(data);
+          setTableData(sortedData);
           setCountries(countries);
         });
     };
@@ -109,6 +113,7 @@ function App() {
           <h3> Worldwide new cases</h3>
           {/* Table */}
           {/* Graph */}
+          <LineGraph/>
 
         </CardContent>
       </Card>
